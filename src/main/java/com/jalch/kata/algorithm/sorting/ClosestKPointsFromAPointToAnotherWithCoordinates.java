f59e1b2dd86c71993fb422c0639913a778f9b42d
jLandazuri <jorge.landazuri@lastminute.com>
2018-10-13 16:17:00 +0100

    ClosestKPointsFromAPointToAnotherWithCoordinates using a full sort and max heap alternative, with some time complexity analysis and tests -> max heap (Priority Queue) version is faster for all values of k and n.

--- src/main/java/com/jalch/kata/algorithm/sorting/ClosestKPointsFromAPointToAnotherWithCoordinates.java
+++ src/main/java/com/jalch/kata/algorithm/sorting/ClosestKPointsFromAPointToAnotherWithCoordinates.java
@@ -1,19 +1,105 @@
 package com.jalch.kata.algorithm.sorting;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.stream.Collectors;
+
+import static java.lang.Double.compare;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+import static java.util.Arrays.asList;
+import static java.util.Collections.EMPTY_LIST;
+import static java.util.Comparator.naturalOrder;
 
 class ClosestKPointsFromAPointToAnotherWithCoordinates {
+    //coordinates.size() = n
 
-    static List<List<Integer>> get(int k, List<Integer> position, List<List<Integer>> coordinates) {
-        if(notValidPoints(position, coordinates)) return Collections.EMPTY_LIST;
+    //Time complexity: O(n) + O(n log n) + 0(1) ~ O(n log n)
+    static List<List<Integer>> getByFullSorting(int k, List<Integer> position, List<List<Integer>> coordinates) {
+        if(notValidInput(k, position, coordinates)) return EMPTY_LIST;
 
-        return k <= coordinates.size() ? coordinates.subList(0, k) : coordinates;
+        //O(n)
+        List<CoordinatesWithDistance> coordinatesWithDistances = getCoordinatesWithDistance(position, coordinates);
+
+        //https://stackoverflow.com/questions/31301471/big-o-complexity-of-java-util-stream-streamt-sorted
+        //O(n log n)
+        List<List<Integer>> sortedCoordinates = coordinatesWithDistances.stream().sorted(naturalOrder())
+                .map(CoordinatesWithDistance::getCoordinates)
+                .collect(Collectors.toList());
+
+        return k <= sortedCoordinates.size()
+                ? sortedCoordinates.subList(0, k) //O(1)
+                : sortedCoordinates;
     }
 
-    private static boolean notValidPoints(List<Integer> position, List<List<Integer>> coordinates) {
-        return position == null || position.isEmpty() || coordinates == null || coordinates.isEmpty();
+    //Time complexity: O(n) + O(k) + 0((n-k) log k) + O(k log k)
+    // ~ 0((n-k) log k) if n-k > 0
+    // ~ 0(k log k) if n == k
+    //      => But would tend to be faster than full sort one as the max heap is sorted by levels at least (see test)
+    static List<List<Integer>> getByUsingMaxHeap(int k, List<Integer> position, List<List<Integer>> coordinates) {
+        if(notValidInput(k, position, coordinates)) return EMPTY_LIST;
+
+        //O(n)
+        List<CoordinatesWithDistance> coordinatesWithDistances = getCoordinatesWithDistance(position, coordinates);
+
+        //0(k)
+        PriorityQueue<CoordinatesWithDistance> maxHeap = new PriorityQueue<>(naturalOrder());
+        int resultSize = k <= coordinates.size() ? k : coordinates.size();
+        for(int i = 0; i < resultSize; i++) {
+            maxHeap.add(coordinatesWithDistances.get(i));
+        }
+
+        //0((n-k) log k) -> Iterating (n-k) times over a heap of size k.
+        for(int i = resultSize; i < coordinatesWithDistances.size(); i++){
+            CoordinatesWithDistance current = coordinatesWithDistances.get(i);
+            if(maxHeap.peek().getDistance() > current.getDistance()){
+                maxHeap.poll();
+                maxHeap.add(current);
+            }
+        }
+        //Need to sort the result, as can't guarantee order with nodes at same level.
+        //O(k log k)
+        return maxHeap.stream().sorted(naturalOrder()).map(i -> i.getCoordinates()).collect(Collectors.toList());
     }
 
+    private static boolean notValidInput(int k , List<Integer> position, List<List<Integer>> coordinates) {
+        return k < 1 || position == null || position.size() != 2
+                || coordinates == null || coordinates.isEmpty()
+                || coordinates.stream().anyMatch(c -> c.size() != 2);
+    }
+
+    private static List<CoordinatesWithDistance> getCoordinatesWithDistance(List<Integer> position, List<List<Integer>> coordinates) {
+        return coordinates.stream()
+                .map(c -> new CoordinatesWithDistance(c.get(0), c.get(1), position.get(0), position.get(1)))
+                .collect(Collectors.toList());
+    }
+
+    static class CoordinatesWithDistance implements Comparable<CoordinatesWithDistance> {
+        private final int x;
+        private final int y;
+        private final double distance;
+
+        CoordinatesWithDistance(int x, int y, int xFrom, int yFrom) {
+            this.x = x;
+            this.y = y;
+            this.distance = sqrt(pow(xFrom-x, 2) + pow(yFrom-y, 2));
+        }
+
+        double getDistance() {
+            return distance;
+        }
+
+        List<Integer> getCoordinates() {
+            return asList(x,y);
+        }
+
+        @Override
+        public int compareTo(CoordinatesWithDistance other) {
+            //ascending order
+            return compare(this.distance, other.getDistance());
+            //descending order
+            //return compare(other.getDistance(), this.distance);
+        }
+    }
 
 }
--- src/test/java/com/jalch/kata/algorithm/sorting/ClosestKPointsFromAPointToAnotherWithCoordinatesTest.java
+++ src/test/java/com/jalch/kata/algorithm/sorting/ClosestKPointsFromAPointToAnotherWithCoordinatesTest.java
@@ -2,35 +2,153 @@ package com.jalch.kata.algorithm.sorting;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
-import static com.jalch.kata.algorithm.sorting.ClosestKPointsFromAPointToAnotherWithCoordinates.get;
+import static com.jalch.kata.algorithm.sorting.ClosestKPointsFromAPointToAnotherWithCoordinates.getByFullSorting;
+import static com.jalch.kata.algorithm.sorting.ClosestKPointsFromAPointToAnotherWithCoordinates.getByUsingMaxHeap;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
 import static java.util.Arrays.asList;
 import static java.util.Collections.EMPTY_LIST;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
+@SuppressWarnings("Duplicates")
 public class ClosestKPointsFromAPointToAnotherWithCoordinatesTest {
 
     private static final int ANY_NUMBER = 13;
-    private static final List<Integer> ANY_POINT = asList(3,5);
-    private static final List<List<Integer>> ANY_POINTS = asList(ANY_POINT, ANY_POINT, ANY_POINT);
+    private static final List<Integer> ANY_POINT = asList(0, 0);
+    private static final List<List<Integer>> ANY_POINTS = asList(asList(3,6), asList(2,6), asList(0,6));
 
     @Test
     public void null_or_empty_coordinates_input() {
-        assertThat(get(ANY_NUMBER, null, EMPTY_LIST).size(), is(0));
-        assertThat(get(ANY_NUMBER, EMPTY_LIST, null).size(), is(0));
+        assertThat(getByFullSorting(ANY_NUMBER, null, EMPTY_LIST).size(), is(0));
+        assertThat(getByFullSorting(ANY_NUMBER, EMPTY_LIST, null).size(), is(0));
+
+        assertThat(getByUsingMaxHeap(ANY_NUMBER, null, EMPTY_LIST).size(), is(0));
+        assertThat(getByUsingMaxHeap(ANY_NUMBER, EMPTY_LIST, null).size(), is(0));
     }
 
     @Test
     public void zero_or_negative_k_required() {
-        assertThat(get(0, ANY_POINT, ANY_POINTS).size(), is(0));
-        assertThat(get(0, ANY_POINT, ANY_POINTS).size(), is(0));
-        assertThat(get(0, ANY_POINT, ANY_POINTS).size(), is(0));
+        assertThat(getByFullSorting(0, ANY_POINT, ANY_POINTS).size(), is(0));
+        assertThat(getByFullSorting(-1, ANY_POINT, ANY_POINTS).size(), is(0));
+
+        assertThat(getByUsingMaxHeap(0, ANY_POINT, ANY_POINTS).size(), is(0));
+        assertThat(getByUsingMaxHeap(-1, ANY_POINT, ANY_POINTS).size(), is(0));
+    }
+
+    @Test
+    public void coordinates_incorrect_size() {
+        assertThat(getByFullSorting(ANY_NUMBER, asList(3), ANY_POINTS).size(), is(0));
+        assertThat(getByFullSorting(ANY_NUMBER, asList(3, 4, 5), ANY_POINTS).size(), is(0));
+        assertThat(getByFullSorting(ANY_NUMBER, ANY_POINT, asList(asList(3))).size(), is(0));
+        assertThat(getByFullSorting(ANY_NUMBER, ANY_POINT, asList(asList(3, 4, 5))).size(), is(0));
+
+        assertThat(getByUsingMaxHeap(ANY_NUMBER, asList(3), ANY_POINTS).size(), is(0));
+        assertThat(getByUsingMaxHeap(ANY_NUMBER, asList(3, 4, 5), ANY_POINTS).size(), is(0));
+        assertThat(getByUsingMaxHeap(ANY_NUMBER, ANY_POINT, asList(asList(3))).size(), is(0));
+        assertThat(getByUsingMaxHeap(ANY_NUMBER, ANY_POINT, asList(asList(3, 4, 5))).size(), is(0));
     }
 
     @Test
     public void more_k_request_than_available() {
-        assertThat(get(4, ANY_POINT, ANY_POINTS).size(), is(3));
+        List<List<Integer>> result = getByFullSorting(4, ANY_POINT, ANY_POINTS);
+        assertThat(result.size(), is(3));
+        assertThat(result.get(0), is(asList(0,6)));
+        assertThat(result.get(1), is(asList(2,6)));
+        assertThat(result.get(2), is(asList(3,6)));
+
+        result = getByUsingMaxHeap(4, ANY_POINT, ANY_POINTS);
+        assertThat(result.size(), is(3));
+        assertThat(result.get(0), is(asList(0,6)));
+        assertThat(result.get(1), is(asList(2,6)));
+        assertThat(result.get(2), is(asList(3,6)));
+    }
+
+    @Test //Executed in ~1,6 seconds in average (2.5 GHz Intel Core i7)
+    public void k_items_sorted_by_distance_by_full_coordinates_sorting() {
+        List<Integer> randomPosition = getRandomPosition();
+        List<List<Integer>> result = getByFullSorting(5, randomPosition, getRandomCoordinates(10));
+        assertSortedByDistanceFor(result, randomPosition);
+
+        randomPosition = getRandomPosition();
+        result = getByFullSorting(500, randomPosition, getRandomCoordinates(100_000));
+        assertSortedByDistanceFor(result, randomPosition);
+
+        randomPosition = getRandomPosition();
+        result = getByFullSorting(5000, randomPosition, getRandomCoordinates(1_000_000));
+        assertSortedByDistanceFor(result, randomPosition);
+    }
+
+    @Test //Executed in ~2.4 seconds in average (2.5 GHz Intel Core i7)
+    public void all_items_sorted_by_distance_by_full_coordinates_sorting() {
+        List<Integer> randomPosition = getRandomPosition();
+        List<List<Integer>> result = getByFullSorting(10, randomPosition, getRandomCoordinates(10));
+        assertSortedByDistanceFor(result, randomPosition);
+
+        randomPosition = getRandomPosition();
+        result = getByFullSorting(100_000, randomPosition, getRandomCoordinates(100_000));
+        assertSortedByDistanceFor(result, randomPosition);
+
+        randomPosition = getRandomPosition();
+        result = getByFullSorting(1_000_000, randomPosition, getRandomCoordinates(1_000_000));
+        assertSortedByDistanceFor(result, randomPosition);
+    }
+
+    @Test //Executed in ~0.4 seconds in average (2.5 GHz Intel Core i7)
+    public void k_items_sorted_by_distance_by_using_max_heap() {
+        List<Integer> randomPosition = getRandomPosition();
+        List<List<Integer>> result = getByUsingMaxHeap(5, randomPosition, getRandomCoordinates(10));
+        assertSortedByDistanceFor(result, randomPosition);
+
+        randomPosition = getRandomPosition();
+        result = getByUsingMaxHeap(500, randomPosition, getRandomCoordinates(100_000));
+        assertSortedByDistanceFor(result, randomPosition);
+
+        randomPosition = getRandomPosition();
+        result = getByUsingMaxHeap(5000, randomPosition, getRandomCoordinates(1_000_000));
+        assertSortedByDistanceFor(result, randomPosition);
+    }
+
+    @Test //Executed in ~0,9 seconds in average (2.5 GHz Intel Core i7)
+    public void all_items_sorted_by_distance_by_using_max_heap() {
+        List<Integer> randomPosition = getRandomPosition();
+        List<List<Integer>> result = getByUsingMaxHeap(10, randomPosition, getRandomCoordinates(10));
+        assertSortedByDistanceFor(result, randomPosition);
+
+        randomPosition = getRandomPosition();
+        result = getByUsingMaxHeap(100_000, randomPosition, getRandomCoordinates(100_000));
+        assertSortedByDistanceFor(result, randomPosition);
+
+        randomPosition = getRandomPosition();
+        result = getByUsingMaxHeap(1_000_000, randomPosition, getRandomCoordinates(1_000_000));
+        assertSortedByDistanceFor(result, randomPosition);
+    }
+
+    private List<Integer> getRandomPosition() {
+        return asList(new Random().nextInt(), new Random().nextInt());
+    }
+
+    private void assertSortedByDistanceFor(List<List<Integer>> result, List<Integer> randomPosition) {
+        for (int i = 0; i < result.size() - 1; i++) {
+            boolean success = distance(randomPosition, result.get(i)) <= distance(randomPosition, result.get(i + 1));
+            assertTrue(success);
+        }
+    }
+
+    private double distance(List<Integer> from, List<Integer> to) {
+        return sqrt(pow(from.get(0) - to.get(0), 2) + pow(from.get(1) - to.get(1), 2));
+    }
+
+    private List<List<Integer>> getRandomCoordinates(int numberOfCoordinates) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < numberOfCoordinates; i++) {
+            result.add(Arrays.asList(new Random().nextInt(), new Random().nextInt()));
+        }
+        return result;
     }
 }
